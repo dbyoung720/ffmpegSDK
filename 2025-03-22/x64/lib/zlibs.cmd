a13edd7858f47c9724e30672af8d68b4e186dd0a
dbyoung <dbyoung@sina.com>
2025-03-22 23:49:14 +0800

    add 2025-03-22

---
+++ 2025-03-22/x64/lib/zlibs.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+for %%G in (Community,Professional,Enterprise) do (
+  if exist "C:\Program Files (x86)\Microsoft Visual Studio\2022\%%G" (
+    set "VSInstallPath=C:\Program Files (x86)\Microsoft Visual Studio\2022\%%G"
+  )
+  if exist "C:\Program Files\Microsoft Visual Studio\2022\%%G" (
+    set "VSInstallPath=C:\Program Files\Microsoft Visual Studio\2022\%%G"
+  )
+)
+if "%VSInstallPath%" == "" (
+  echo "Visual Studio 2022 not found"
+  pause
+  exit 1
+)
+
+call "%VSInstallPath%\VC\Auxiliary\Build\vcvars64.bat"
+
+cd..
+cd bin
+
+lib /def:"..\lib\avcodec-61.def"   /machine:i386 /out:"..\lib\avcodec-61.lib"
+lib /def:"..\lib\avdevice-61.def"  /machine:i386 /out:"..\lib\avdevice-61.lib"
+lib /def:"..\lib\avfilter-10.def"   /machine:i386 /out:"..\lib\avfilter-10.lib"
+lib /def:"..\lib\avformat-61.def"  /machine:i386 /out:"..\lib\avformat-61.lib"
+lib /def:"..\lib\avutil-59.def"    /machine:i386 /out:"..\lib\avutil-59.lib"
+lib /def:"..\lib\postproc-58.def"  /machine:i386 /out:"..\lib\postproc-58.lib"
+lib /def:"..\lib\swresample-5.def" /machine:i386 /out:"..\lib\swresample-5.lib"
+lib /def:"..\lib\swscale-8.def "   /machine:i386 /out:"..\lib\swscale-8.lib"

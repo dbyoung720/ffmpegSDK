e20ba211150ce1ef5e931c9529fa833d7e3b06c8
dbyoung <dbyoung@sina.com>
2025-04-07 08:46:27 +0800

    update

--- zpp.cmd
+++ zpp.cmd
@@ -8,6 +8,7 @@ rem 2025-04-03\x64\bin\ffplay -hide_banner -window_title "dxva2 play" -x 800 -y 
 rem 2025-04-03\x64\bin\ffplay -hide_banner -window_title "dxva2 play" -fs -hwaccel d3d11va F:\VIDEO\Film\Film\x265.mp4
 rem 2025-04-03\x64\bin\ffplay -hide_banner -window_title "dxva2 play" -fs -hwaccel cuda F:\VIDEO\Film\Film\x265.mp4
 
-
+rem 2025-04-03\x64\bin\ffplay -rtsp_transport tcp -x 800 -y 600 -fflags nobuffer -analyzeduration 1000000 -probesize 1000000 rtsp://admin:@192.168.110.40/Streaming/Channels/101
+rem 2025-04-03\x64\bin\ffplay -rtsp_transport tcp -x 800 -y 600 -analyzeduration 1000000 -probesize 1000000 -tune zerolatency -preset ultrafast rtsp://admin:@192.168.110.40/Streaming/Channels/101
  
 pause
